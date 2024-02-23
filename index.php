853a40fe669773346cc74d82b9582468fa43dc7f
André Vinícius <14371772+andrevlima@users.noreply.github.com>
2024-02-23 16:24:30 +0000

    Caching mechanism + refactoring

--- index.php
+++ index.php
@@ -5,106 +5,127 @@
 * It uses the investing.com as data source and using "web crawling" techniques.
 * Revelant data is captured and returned in a JSON.
 * 
-* There is no guarantees about the availability or estability of this API, changes 
-* can be done in source page that can result in a crash of script.
-*
+* By default cache is used in order make the API faster.
+* 
 * @author André Lima <andrelimamail@gmail.com>
 * @license MIT
 * @version 1.0
 */
-header("Content-Type: application/json");
 
-include_once(getcwd() . "/vendor/kub-at/php-simple-html-dom-parser/src/KubAT/PhpSimple/HtmlDomParser.php");
+include_once(getcwd() . "/vendor/autoload.php");
 use KubAT\PhpSimple\HtmlDomParser;
 
+class EconomicCalendarAPI {
+    private $cacheSuffix = "_api-cache.json";
+    private $minGapFromNextEvent = 300; // If next event is timed for the next 3 minutes, cache won't be used
+    private $dataSourceUrl = "https://sslecal2.forexprostools.com/";
+    private $cacheFile = false; // File path to cache
 
-function sanitize($str) {
-	return trim(str_replace("&nbsp;", "", $str));
-}
+    public function fetchEvents() {
+        header("Content-Type: application/json");
+        
+        $this->setupCacheFile();
 
-function findFirstCacheFile() {
-    $suffix = "_api-cache.json";
-    $files = glob("./*" . $suffix);
-    return $files ? $files[0] : null;
-}
-
-function extractTimestampFromFilePath($filePath) {
-    if (preg_match('/(\d+)_api-cache\.json$/', $filePath, $matches)) {
-        return $matches[1]; // The first captured group is the timestamp
+        if ($this->shouldUseCache()) {
+            echo "TRUE:".$this->cacheFile."\n";
+            $this->serveFromCache();
+        } else {
+            echo "false\n";
+            $this->fetchAndCacheData();
+        }
     }
-    return null; // Return null if no match is found
-}
-
-
-function getMinuteDifference($dateString) {
-    $specifiedDate = new DateTime($dateString);
-    $serverDate = new DateTime("now");
-    return ($specifiedDate->getTimestamp() - $serverDate->getTimestamp()) / 60;
-}
-
-
-// Example usage
-$useCache = false;
-$cacheFile = findFirstCacheFile();
-
-//echo $cacheFile . "\n\n";
-if($cacheFile != null) {
-    $nextEventTimeFromCache = extractTimestampFromFilePath($cacheFile);
-    $date = new DateTime();
-    $date->setTimestamp($nextEventTimeFromCache);
-    $timeGapInMinutes = getMinuteDifference($date->format('Y-m-d H:i:s'));
     
-    if($timeGapInMinutes > 3) { // 3 Minutes before any event cache is avoided
-        $useCache = true;
+    private function setupCacheFile() {
+        $this->cacheFile = $this->findFirstCacheFile();
     }
-}
 
+    private function shouldUseCache() {
+        if (!$this->cacheFile) return false;
 
-if($useCache) {
-    $cacheContent = file_get_contents($cacheFile);
-    if($cacheContent !== false) {
-        echo $cacheContent;
+        $cacheTime = $this->extractTimestampFromFilePath($this->cacheFile);
+        echo "\nTIMEGAP:".($cacheTime - time())."\n";
+        return ($cacheTime - time()) > $this->minGapFromNextEvent && (time() - $cacheTime) < $this->minGapFromNextEvent;
+    }
+
+    private function serveFromCache() {
+        echo file_get_contents($this->cacheFile);
         exit();
     }
+
+    private function fetchAndCacheData() {
+        $dom = HtmlDomParser::file_get_html($this->dataSourceUrl);
+        $data = $this->parseDataFromDom($dom);
+        $this->cacheData($data);
+        echo json_encode($data);
+        exit();
+    }
+
+    private function findFirstCacheFile() {
+        $files = glob("./*" . $this->cacheSuffix);
+        return $files ? $files[0] : null;
+    }
+
+    private function extractTimestampFromFilePath($filePath) {
+        if (preg_match('/(\d+)' . preg_quote($this->cacheSuffix) . '$/', $filePath, $matches)) {
+            return (int)$matches[1];
+        }
+        return null;
+    }
+
+    private function getMinuteDifference($dateString) {
+        $specifiedDate = new DateTime($dateString);
+        $serverDate = new DateTime("now");
+        return ($specifiedDate->getTimestamp() - $serverDate->getTimestamp()) / 60;
+    }
+
+    private function sanitize($str) {
+        return trim(str_replace("&nbsp;", "", $str));
+    }
+
+    private function parseDataFromDom($dom) {
+        $elems = $dom->getElementById("#ecEventsTable")->find("tr[id*='eventRowId']");
+        $events = [];
+
+        $nextFutureEvent = null;
+        $nextFutureEventSmallestDif = 9999;
+                
+        foreach($elems as $element) {
+            $date = isset($element->attr["event_timestamp"]) ? $element->attr["event_timestamp"] : null;
+            $name = count($name = $element->find("td.event")) > 0 ? $this->sanitize($name[0]->text()) : null;
+
+            if($date != null) {
+                $minutesDifference = $this->getMinuteDifference($date);
+                if($minutesDifference > 0 && ($nextFutureEvent == null || $minutesDifference < $nextFutureEventSmallestDif)) {
+                    $nextFutureEvent = $date;
+                    $nextFutureEventSmallestDif = $minutesDifference;
+                }
+            }
+            
+            array_push($events, [
+                "economy" => $this->sanitize($element->find("td.flagCur")[0]->text()),
+                "impact" =>  count($element->find("td.sentiment")[0]->find("i.grayFullBullishIcon")),
+                "date" => $date,
+                "name" => $name,
+                "actual" => count($actual = $element->find("td.act")) > 0 ? $this->sanitize($actual[0]->text()) : null,
+                "forecast" => count($forecast = $element->find("td.fore")) > 0 ? $this->sanitize($forecast[0]->text()) : null,
+                "previous" => count($previous = $element->find("td.prev")) > 0 ? $this->sanitize($previous[0]->text()) : null,
+            ]);
+        }
+
+        return [
+            "next_event_date"=> $nextFutureEvent,
+            "events"=> $events,
+        ];
+    }
+
+    private function cacheData($data) {
+        if ($this->cacheFile) unlink($this->cacheFile);
+        
+        $nextEventDate = new DateTime($data['next_event_date']);
+        $cacheFileName = $nextEventDate->getTimestamp() . $this->cacheSuffix;
+        file_put_contents($cacheFileName, json_encode($data['events']));
+    }
 }
 
-
-
-$dom = HtmlDomParser::file_get_html("https://sslecal2.forexprostools.com/", false, null, 0);
-$elems = $dom->getElementById("#ecEventsTable")->find("tr[id*='eventRowId']");
-$data = [];
-
-$nextFutureEvent = null;
-$nextFutureEventSmallestDif = 9999;
-$nextFutureEventName = "";
-
-foreach($elems as $element) {
-	$date = isset($element->attr["event_timestamp"]) ? $element->attr["event_timestamp"] : null;
-	$name = count($name = $element->find("td.event")) > 0 ? sanitize($name[0]->text()) : null;
-
-	if($date != null) {
-		$minutesDifference = getMinuteDifference($date);
-		if($minutesDifference > 0 && ($nextFutureEvent == null || $minutesDifference < $nextFutureEventSmallestDif)) {
-			$nextFutureEvent = $date;
-			$nextFutureEventSmallestDif = $minutesDifference;
-			$nextFutureEventName = $name;
-		}
-	}
-	
-    array_push($data, [
-        "economy" => sanitize($element->find("td.flagCur")[0]->text()),
-        "impact" =>  count($element->find("td.sentiment")[0]->find("i.grayFullBullishIcon")),
-	    "date" => $date,
-	    "name" => $name,
-	    "actual" => count($actual = $element->find("td.act")) > 0 ? sanitize($actual[0]->text()) : null,
-	    "forecast" => count($forecast = $element->find("td.fore")) > 0 ? sanitize($forecast[0]->text()) : null,
-	    "previous" => count($previous = $element->find("td.prev")) > 0 ? sanitize($previous[0]->text()) : null,
-	]);
-}
-
-//echo ($nextFutureEventSmallestDif / 60). "\n" . $nextFutureEventName . "\n";
-//echo $nextFutureEvent;
-$nextFutureEventDate = new DateTime($nextFutureEvent);
-echo json_encode($data);
-file_put_contents($nextFutureEventDate->getTimestamp() . "_api-cache.json", json_encode($data));
-exit();
+$api = new EconomicCalendarAPI();
+$api->fetchEvents();
